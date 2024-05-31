3ea1e035077e9ae2b804928460127768b7ab7f8c
Subhankar4901 <Subhankarh701@iitkgp.ac.in>
2024-05-31 11:50:06 +0530

    Removed the upper_bound component from list_insert() functionality. it's still O(n).

--- utils/list_utils.c
+++ utils/list_utils.c
@@ -3,10 +3,10 @@
 int heap_chunk_query(HeapChunk_List*list,void*ptr)
 {
     int lo=0;
-    int hi=list->len;
+    int hi=list->len-1;
     while(lo<=hi)
     {
-        int mid=lo+(hi-lo)/2;
+        int mid=(hi+lo)/2;
         HeapChunk cur_chunk=list->Heap_Chunks[mid];
 
         if(cur_chunk.start==ptr)
@@ -18,34 +18,20 @@ int heap_chunk_query(HeapChunk_List*list,void*ptr)
     }
     return -1;
 }
-int insert_pos(HeapChunk_List*list,HeapChunk*ptr)
-{
-    int lo=0;
-    int hi=list->len;
-    while(lo<hi)
-    {
-        int mid=(hi+lo)/2;
-        HeapChunk cur_chunk=list->Heap_Chunks[mid];
-        if(cur_chunk.start<=ptr->start)
-        {
-        hi=mid-1;
-        }
-        else
-        lo=mid+1;
-    }
-    return lo;
-}
 void * heap_chunk_insert(HeapChunk_List*list,HeapChunk*ptr)
 {
     if(list->len==HEAP_CHUNK_CAP)
       return NULL;
-    int idx=insert_pos(list,ptr->start);
-    for(size_t i=list->len;i>idx;i--)
+    list->Heap_Chunks[list->len]=*ptr;
+    int cur_idx=(list->len);
+    while(cur_idx>0 && ptr->start<(list->Heap_Chunks[cur_idx-1].start))
     {
-        list->Heap_Chunks[i]=list->Heap_Chunks[i-1];
+        HeapChunk temp=list->Heap_Chunks[cur_idx];
+        list->Heap_Chunks[cur_idx]=list->Heap_Chunks[cur_idx-1];
+        list->Heap_Chunks[cur_idx-1]=temp;
+        cur_idx--;
     }
-    list->Heap_Chunks[idx]=*ptr;
-    list->len+=1;
+    list->len++;
     return ptr;
 
 }
