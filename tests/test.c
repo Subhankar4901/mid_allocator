e963a703f39de8c2f2563ea5092129dddbe88d80
Subhankar4901 <subhankarh701@gmail.com>
2024-10-28 23:28:39 +0530

    - Made more robust testing feature that can test specific functions. 	- if the function retuns true then test passes. 	- if false then test fails.

--- tests/test.c
+++ tests/test.c
@@ -1,29 +1,18 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include "../allocator/allocator.h"
-int main()
-{
-    init_allocator();
-    for(int i=1;i<=10;i++)
-    {
-        void * alloc_res=heap_alloc(i);
-        if(alloc_res==NULL)
-        {
-            printf("Failed to allocate %d bytes",i);
-            return 1;
-        }
-        if(i%2==0)
-        {
-            bool free_res=heap_free(alloc_res);
-            if(!free_res)
-            {
-                printf("Failed to allocate %p location",alloc_res);
-                return 1;
-            }
-        }
+#include "test.h"
+void test(bool res,char * msg){
+    if(res)
+    printf("\033[1;32m[PASSED]\033[0m %s\n",msg);
+    else{
+        printf("\033[1;31m[FAILED]\033[0m %s\n",msg);
+        exit(EXIT_FAILURE);
     }
-    printf("Allocated Chunks : \n");
-    heap_chunk_dump(&Allocated_Heap_Chunks);
-    printf("Freed Chunks : \n");
-    heap_chunk_dump(&Freed_Heap_Chunks);
-    return 0;
+}
+int main(){
+    init_allocator(0);
+    test(tree_test(),"Tree test");
+    test(linked_list_test(),"Linked list test");
+    test(array_test(),"Array test");
 }
---
+++ tests/test.h
@@ -0,0 +1,6 @@
+#ifndef TEST_H
+#define TEST_H
+bool tree_test(void);
+bool linked_list_test(void);
+bool array_test(void);
+#endif
