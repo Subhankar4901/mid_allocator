9cd7fb97a28f1f2ed8583dd8b058fe51d371e4de
Subhankar4901 <subhankarh701@gmail.com>
2024-10-28 23:12:15 +0530

    - Updated the HeapChunk struct's start pointer to void * start from uintptr_t* strat. - Updated the HeapChunk_List struct's HeapChunks array from HeamChunk HeapChunks[CAP] to HeapChunk * HeapChunks - Removed the constants HEAP_CAP and HEAP_CHUNK_CAP. it's now stored in allocator/allocator.c - Added LogLeveltoStringConsole for console printing as it needs to print colour using ANSI code. 	- it's different from LogLeveltoString as it's for syslogs. - Removed Heap_Chunk_Dump() to print memory chunks as it's shifted to allocator/allocator.c

--- utils/utils.h
+++ utils/utils.h
@@ -1,11 +1,10 @@
 #ifndef UTILS_H
 #define UTILS_H
 #include<stdbool.h>
-#define HEAP_SIZE 640000 //640KB;
-#define HEAP_CHUNK_CAP 4000 
+#include<stdint.h>
 #define ALLOC_ERR_MSG "Failed to allocate memory."
 #define DEALLOC_ERR_MSG "Failed to de-allocate memory."
-#define CHECK_SYS_LOG_MSG "[INFO] Check System logs at /var/log/syslog or /var/log/messages (CentOS,RHEL) for detailed error.\n"
+#define CHECK_SYS_LOG_MSG "\n[INFO] Check System logs at /var/log/syslog or /var/log/messages (CentOS,RHEL) for detailed error.\n"
 typedef struct
 {
     void* start;
@@ -13,7 +12,7 @@ typedef struct
 }HeapChunk;
 typedef struct
 {
-    HeapChunk Heap_Chunks[HEAP_CHUNK_CAP];
+    HeapChunk *Heap_Chunks;
     size_t len;
 }HeapChunk_List;
 typedef enum{
@@ -22,12 +21,12 @@ typedef enum{
     ERROR,
     UNKNOWN
 }LogLevel;
-void heap_chunk_dump(HeapChunk_List*list);
 int heap_chunk_query(HeapChunk_List* list,void*ptr);
-void* heap_chunk_insert(HeapChunk_List* list,HeapChunk*ptr);
+void* heap_chunk_insert(HeapChunk_List* list,HeapChunk*ptr,size_t cap);
 HeapChunk heap_chunk_remove(HeapChunk_List* list,void*ptr,int index);
 void merge_freed_chunks(HeapChunk_List * list);
 char * LogLevelToString(LogLevel level);
+char * LogLevelToStringConsole(LogLevel level);
 void Log_Msg(LogLevel level,const char * msg,const char * file,int line);
 void Log_Sys(LogLevel level,const char * msg,const char * file,int line);
 #define LOG(level,msg) Log_Msg(level,msg,__FILE__,__LINE__)
